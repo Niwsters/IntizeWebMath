35d6d9c78ba22ce9d47d6e73077cf1d04cd8b255
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2013-10-06 23:19:15 +0200

    Initializing repository

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Seb's Mathematics Exercises</title>
+	</head>
+<body>
+<h2>Math Exercises</h2>
+<nav class="math-navigation">
+	<button class="math-choose-addition" />
+</nav>
+<div class="math-exercise">
+	
+</div>
+</body>
+</html>
